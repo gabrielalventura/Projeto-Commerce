ccac62d46b8ede5cd3a8dae67ba12c95e11063b1
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-04-17 19:09:39 -0300

    Feat: Realiza desafio 2

---
+++ challenges/desafio21.js
@@ -0,0 +1,12 @@
+// 1 indica que será retirado o ultimo
+db.produtos.updateOne({
+  nome: "Cheddar McMelt",
+},
+{
+  $pop: { ingredientes: 1 },
+});
+
+db.produtos.find({},
+  {
+    nome: 1, ingredientes: 1, _id: 0,
+  });
