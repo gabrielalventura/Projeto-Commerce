60f0d190eeb04db43c2c4870a3688f2aa1c44cab
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-04-17 15:22:41 -0300

    Fix: Corrige desafio 3, pois o teste falha quando se omite o parametro query

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -1,3 +1,3 @@
 // db.collection.find(<query>).limit(<número>)
 
-db.produtos.find({ nome: 1, vendidos: 1, _id: 0 }).sort({ vendidos: -1 }).limit(1);
+db.produtos.find({}, { nome: 1, vendidos: 1, _id: 0 }).sort({ vendidos: -1 }).limit(1);
