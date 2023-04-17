7b3cc11411bf4e045880743f46dd3948e571356f
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-04-17 19:04:00 -0300

    Feat: Realiza desafio 20

---
+++ challenges/desafio20.js
@@ -0,0 +1,12 @@
+// -1 indica que será retirado o primeiro
+db.produtos.updateOne({
+  nome: "Quarteirão com Queijo",
+},
+{
+  $pop: { ingredientes: -1 },
+});
+
+db.produtos.find({},
+  {
+    nome: 1, ingredientes: 1, _id: 0,
+  });
